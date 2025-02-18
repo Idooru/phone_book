7b42865a57b61720b405eb3633643d399d8c54f6
idooru <shere1765@gmail.com>
2025-02-18 14:06:42 +0900

    이름 변경

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:phone_book/core/themes/theme_data.dart';
 import 'package:phone_book/core/utils/get_it_initializor.dart';
 import 'package:phone_book/presentation/state/phonebook_provider.dart';
-import 'package:phone_book/presentation/pages/phone_book.dart';
+import 'package:phone_book/presentation/pages/phonebook_page.dart';
 import 'package:provider/provider.dart';
 
 void main() {
